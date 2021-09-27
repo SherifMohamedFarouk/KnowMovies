b6198d2048b722adb876228d0bcb7778bc991b91
Shiro <sherifmohamed2004@gmail.com>
2021-09-27 17:02:02 +0200

    developer branch

--- lib/config/palette.dart
+++ lib/config/palette.dart
@@ -4,5 +4,5 @@ class Palette{
   static const Color lightGreen = Color(0x14c0b68c);
   static const Color shadeyYellow = Color(0x5ef8cf23);
   static const Color black = Color(0xFF222629);
-  static const Color toggleColor = Color(0xFFc0b68c);
+  // static Color get toggleColor => Color(0xFFc0b68c);
 }
